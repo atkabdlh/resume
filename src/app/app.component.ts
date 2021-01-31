5433c654e8ebfffce1e9b452f782d868db7d3d58
atikah.abdullah <atikah_109@yahoo.com.my>
2021-01-31 23:59:10 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Data from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+
+  fullname: string = Data.fullname;
+  phoneNo: string = Data.phoneNo;
+  email: string = Data.email;
+  websites: string[] = Data.websites;
+  statements: string[] = Data.personalStatement;
+
+  skills: object[] = Data.skillsList;
+  workHistory: object[] = Data.workHistory;
+  educations: object[] = Data.education;
+
+  subTitle: string[] = ['Skills', 'Work History', 'Education'];
+
+  // tslint:disable-next-line:typedef
+  printPdf() {
+    const link = document.createElement('a');
+    link.download = 'resume.pdf';
+    link.href = 'assets/resume.pdf';
+    link.click();
+  }
+
+  // tslint:disable-next-line:typedef
+  printCoverLetter() {
+    const link = document.createElement('a');
+    link.download = 'cover_letter.pdf';
+    link.href = 'assets/cover_letter.pdf';
+    link.click();
+  }
 }
---
+++ src/app/history/history.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-history',
+  templateUrl: './history.component.html',
+  styleUrls: ['./history.component.scss']
+})
+export class HistoryComponent implements OnInit {
+  @Input() title: string;
+  @Input() historyObj: object[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
